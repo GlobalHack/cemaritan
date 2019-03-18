b1e9001aeb71e160c85a07f9aacd28a9f113e6a9
Kyle Sykes <kyle.a.sykes@gmail.com>
2019-03-18 16:54:27 -0500

    Begin adding database access functionality that we need

--- library/connection.py
+++ library/connection.py
@@ -1,23 +1,26 @@
 # functions for converting from sql query results to JSON for API
 # ie: SQL Results <-> JSON conversions
+import sqlite3
 
 
-class Connection:
-    """For subclassing to create new connections to databases
-    
-    """
-
-    def __init__(self, *args, **kwargs):
-        pass
+class SQLite:
+    def __init__(self, db_path, *args, **kwargs):
+        self._db_path = db_path
+        self.connection = sqlite3.connect(self._db_path)
 
     def query(self, query: str):
-        """Make query to database
+        """[summary]
         
         Parameters
         ----------
         query : str
-            SQL query string
+            [description]
         
         """
+        try:
+            response = self.connection.execute(query)
+            self.connection.commit()
+            return response
+        except Exception as e:
+            print(e)
 
-        pass
--- library/database.py
+++ library/database.py
@@ -1,23 +1,86 @@
 # functions for actually making the database calls
+from library.connection import SQLite
+from sqlite3 import Connection
+from typing import List, Tuple
+
+# Sample "create" queries
+"""
+add new organization
+INSERT INTO Organizations (Name, CreatedDate) VALUES (_name_, _createddate_)
+
+add new data mapping
+INSERT INTO DataMappings (Organization, Name, MappingInfo) VALUES ((select Organization from Users where Users.UID = 1), _name_, _mapping_info_);
+
+add a new connection
+INSERT INTO Connections (Organization, Name, CreatedDate, CreatedBy, Type, ConnectionInfo) 
+                VALUES ((select Organization from Users where Users.UID = 1), _name_, _createddate_, createdby_, _type_, _connecitoninfo_);
+
+for deleting user
+DELETE from Users where Users.UID=6
+"""
+
+def get_rows_by_organization(
+    table_name: str, connection: Connection, organization_id: str
+) -> List[Tuple]:
+    """Returns list of tuples where each tuple is a row in the database
+    
+    Parameters
+    ----------
+    table_name : 
+        Name of table in DB
+    connection
+        Connection to DB
+    organization_id
+        Id of Organization
+    
+    Returns
+    -------
+    List[Tuple]
+        List of tuples representing each row of the response of the query
+    """
+    query = f"select * from {table_name} where {table_name}.organization = (select organization from users where users.UID = {organization_id})"
+    return connection.query(query).fetchall()
 
 
-def get_organization(id):
-    pass
+def get_connections(connection, organization_id):
+    return get_rows_by_organization(
+        table_name="connections", connection=connection, organization_id=organization_id
+    )
+
+
+def get_transfers(connection, organization_id):
+    return get_rows_by_organization(
+        table_name="transfers", connection=connection, organization_id=organization_id
+    )
+
+
+def get_users(connection, organization_id):
+    return get_rows_by_organization(
+        table_name="users", connection=connection, organization_id=organization_id
+    )
+
+
+def get_data_mappings(connection, organization_id):
+    return get_rows_by_organization(
+        table_name="datamappings", connection=connection, organization_id=organization_id
+    )
+
+
+def get_organizations(connection, organization_id):
+    query = f"select * from organizations where organizations.uid = '{organization_id}'"
+    return connection.query(query).fetchall()
+
+
+def create_organization(connection, name, created_date):
+    query = f"INSERT INTO Organizations (Name, CreatedDate) VALUES ('{name}', '{created_date}')"
+    connection.query(query)
 
 
 def update_organization(id):
     pass
 
 
-def create_organization(id):
-    pass
-
-
 def delete_organization(id):
-    pass
-
-
-def get_user(id):
     pass
 
 
@@ -33,10 +96,6 @@ def delete_user(id):
     pass
 
 
-def get_transfers(id):
-    pass
-
-
 def update_transfer(id):
     pass
 
@@ -49,10 +108,6 @@ def delete_transfer(id):
     pass
 
 
-def get_connections(id):
-    pass
-
-
 def update_connection(id):
     pass
 
@@ -62,10 +117,6 @@ def create_connection(id):
 
 
 def delete_connection(id):
-    pass
-
-
-def get_data_mapping(id):
     pass
 
 
