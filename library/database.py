486ca0db23e63d5a4723ba4484691c8055516ba5
Kyle Sykes <kyle.a.sykes@gmail.com>
2019-03-21 13:44:43 -0500

    Add in the remainder of create/delete functions

--- library/database.py
+++ library/database.py
@@ -55,6 +55,32 @@ def get_rows_by_organization(
     return labeled_rows
 
 
+def delete_row_by_uid(connection, table_name: str, uid: int):
+    """Delete row in ``table_name`` by ``uid``
+    
+    Parameters
+    ----------
+    connection
+        Connection to database
+    table_name : str
+        Name of table
+    uid : int
+        UID of row to delete
+    
+    Returns
+    -------
+    bool
+        Indicating success or failure of deletion
+    """
+
+    try:
+        query = f"DELETE from {table_name} where {table}.UID={uid}"
+        connection.query(query)
+    except:
+        return False
+    return True
+
+
 def get_connections(connection, organization_id: int):
     """Get connections for ``organization_id``
     
@@ -245,6 +271,99 @@ def create_connection(
     connection.query(query)
 
 
+def create_user(connection, organization_id: int, name: str, created_date: str):
+    """Create user in database
+    
+    Parameters
+    ----------
+    connection
+        Connection to database
+    organization_id : int
+        Organization to crete user in
+    name : str
+        Name of user
+    created_date : str
+        Date Created
+    
+    """
+
+    query = f"INSERT INTO Users (Organization, Name, CreatedDate) VALUES ((select Organization from Users where Users.UID = {user_id}), '{name}', '{created_date}');"
+    connection.query(query)
+
+
+def create_transfer(
+    connection,
+    user_id: int,
+    name: str,
+    created_date: str,
+    created_by: int,
+    organization_id: int,
+    source: int,
+    source_mapping: int,
+    destination: int,
+    destination_mapping: int,
+    start_date_time: str,
+    frequency: str,
+    record_filter: str,
+    active: bool,
+):
+    """Create transfer in database
+    
+    Parameters
+    ----------
+    connection
+        Connection to database
+    user_id : int
+        User creating transfer
+    name : str
+        Name of transfer
+    created_date : str
+        Date transfer was created 
+    created_by : int
+        User creating transfer
+    organization_id : int
+        Organization user belongs to
+    source : int
+        Id of Connection source
+    source_mapping : int
+        Mapping of source system
+    destination : int
+        Id of destination Connection
+    destination_mapping : int
+        Mapping of destination system
+    start_date_time : str
+        Date to start transfer
+    frequency : str
+        Frequency of transfer
+    record_filter : str
+        Filter records parameters
+    active : bool
+        Is connection currently active
+    
+    """
+
+    query = f"INSERT INTO Transfers (Organization, Name, CreatedDate, CreatedBy, Source, SoureMapping, Destination, DestinationMapping, StartDateTime, Frequency, RecordFilter, Active) VALUES ((select Organization from Users where Users.UID = {user_id}), '{name}', '{created_date}', '{created_by}', '{source}', '{source_mapping}', '{destination}', '{destination_mapping}', '{start_date_time}', '{frequency}', '{record_filter}', '{active}');"
+    connection.query(query)
+
+
+def create_organization(connection, name: str, created_date: str):
+    """Create organization in database
+    
+    Parameters
+    ----------
+    connection
+        Connection to database
+    name : str
+        Name of organization
+    created_date : str
+        Date created
+    
+    """
+
+    query = f"INSERT INTO Organizations (Name, CreatedDate) VALUES ('{name}', '{created_date}');"
+    connection.query(query)
+
+
 def delete_user(connection, user_id: int):
     """Delete user with ``user_id`` in database
     
@@ -256,6 +375,60 @@ def delete_user(connection, user_id: int):
         Id of user
     
     """
+    return delete_row_by_uid(connection, "Users", user_id)
 
-    query = f"DELETE from Users where Users.UID={user_id}"
-    connection.query(query)
+
+def delete_organization(connection, organization_id: int):
+    """Delete organization with ``organization_id`` in database
+    
+    Parameters
+    ----------
+    connection
+        Connection to database
+    organization_id : int
+        Id of organization
+    
+    """
+    return delete_row_by_uid(connection, "Organizations", organization_id)
+
+
+def delete_data_mapping(connection, data_mapping_id: int):
+    """Delete data_mapping with ``data_mapping_id`` in database
+    
+    Parameters
+    ----------
+    connection
+        Connection to database
+    data_mapping_id : int
+        Id of data_mapping
+    
+    """
+    return delete_row_by_uid(connection, "DataMappings", data_mapping_id)
+
+
+def delete_connection(connection, connection_id: int):
+    """Delete connection with ``connection_id`` in database
+    
+    Parameters
+    ----------
+    connection
+        Connection to database
+    connection_id : int
+        Id of connection
+    
+    """
+    return delete_row_by_uid(connection, "Connections", connection_id)
+
+
+def delete_transfer(connection, transfer_id: int):
+    """Delete transfer with ``transfer_id`` in database
+    
+    Parameters
+    ----------
+    connection
+        Connection to database
+    transfer_id : int
+        Id of transfer
+    
+    """
+    return delete_row_by_uid(connection, "Transfers", transfer_id)
