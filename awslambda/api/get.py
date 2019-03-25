49be61b3ab019220ad77f07f7ac69f955377c74b
Kyle Sykes <kyle.a.sykes@gmail.com>
2019-03-25 16:13:52 -0500

    Remove serverless comments that I was using as a guide

--- awslambda/api/get.py
+++ awslambda/api/get.py
@@ -1,51 +1,17 @@
 import requests
 
+from library.connection import Aurora
+from library.database import *
+
 
 def connections(event, context):
     try:
         s = event["body"]
 
+        get_connections()
         connection_list = ["org1", "org2"]
 
     except Exception as e:
+        # TODO: Rethink what to return...dumping exceptions is scary for data leakage
         return {"statusCode": 400, "body": "400 Bad Request\n\n" + json.dumps(str(e))}
     return {"statusCode": 200, "body": connection_list}
-
-
-def file_upload(event, context):
-    """Accept a file with either a single or an array of Salesforce objects and convert them."""
-    s = event["body"]
-    s_split = s.split("\n")
-    payload = "\n".join(s_split[3:-2])
-    try:
-        obj = json.loads(payload)
-        if isinstance(obj, list):
-            obj_len = len(obj)
-            list_of_converted_objects = [convert(obj) for obj in obj]
-            # Consolidate into a single set of csv files.
-            consolidated_csv_files = combine_csv_files(
-                csv_files=list_of_converted_objects
-            )
-            # Pass the converted objects to be combined, written to strings, and saved as files in S3.
-            prefix = str(datetime.datetime.now()).replace(" ", "T")
-            save_files_to_s3(
-                bucket=S3_BUCKET_NAME, csv_files=consolidated_csv_files, prefix=prefix
-            )
-        else:
-            obj_len = 1
-            # Convert record
-            converted_data = convert(obj)
-
-            # Save results to S3
-            prefix = str(datetime.datetime.now()).replace(" ", "T")
-            save_files_to_s3(
-                bucket=S3_BUCKET_NAME, csv_files=converted_data, prefix=prefix
-            )
-
-        # Send success notification.
-        send_notification(n=obj_len)
-    except Exception as e:
-        return {"statusCode": 400, "body": "400 Bad Request\n\n" + json.dumps(str(e))}
-
-    return {"statusCode": 200, "body": "Upload successful."}
-
--- awslambda/library/connection.py
+++ awslambda/library/connection.py
@@ -23,3 +23,26 @@ class SQLite:
             return response
         except Exception as e:
             print(e)
+
+
+class Aurora:
+    def __init__(self, db_path, *args, **kwargs):
+        self._db_path = db_path
+        # TODO: Update this connection
+        self.connection = sqlite3.connect(self._db_path)
+
+    def query(self, query: str):
+        """[summary]
+        
+        Parameters
+        ----------
+        query : str
+            [description]
+        
+        """
+        try:
+            response = self.connection.execute(query)
+            self.connection.commit()
+            return response
+        except Exception as e:
+            print(e)
