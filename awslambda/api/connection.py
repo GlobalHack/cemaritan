1c9ebb798ec04576c570cd0450901a2ca5d42a0b
Kyle Sykes <kyle.a.sykes@gmail.com>
2019-04-08 15:58:16 -0500

    updated connections to return sample static payload

--- awslambda/api/connection.py
+++ awslambda/api/connection.py
@@ -1,6 +1,6 @@
 import requests
 
-from library.connection import Aurora
+from library.connection import Postgres
 from library.database import *
 
 # define DB connection here
@@ -10,10 +10,39 @@ def connections(event, context):
     try:
         s = event["body"]
 
-        get_connections()
-        connection_list = ["org1", "org2"]
+        # replace connection_list here with actual database function call
+        connection_list = [
+            {
+                "UID": 1,
+                "Organization": 1,
+                "Name": "SF",
+                "CreatedDate": "2019-03-09 20:42:03",
+                "CreatedBy": 1,
+                "Type": "A",
+                "ConnectionInfo": "{conn string}",
+            },
+            {
+                "UID": 2,
+                "Organization": 1,
+                "Name": "CW",
+                "CreatedDate": "2019-03-10 04:42:03",
+                "CreatedBy": 1,
+                "Type": "B",
+                "ConnectionInfo": "{conn string}",
+            },
+            {
+                "UID": 6,
+                "Organization": 1,
+                "Name": "Secure Download",
+                "CreatedDate": "2019-03-23 20:42:03",
+                "CreatedBy": 0,
+                "Type": "F",
+                "ConnectionInfo": 0,
+            },
+        ]
+        payload = json.dumps(connection_list)
 
     except Exception as e:
         # TODO: Rethink what to return...dumping exceptions is scary for data leakage
         return {"statusCode": 400, "body": "400 Bad Request\n\n" + json.dumps(str(e))}
-    return {"statusCode": 200, "body": connection_list}
+    return {"statusCode": 200, "body": payload}
