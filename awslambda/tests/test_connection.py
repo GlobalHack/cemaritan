de42c2b8e49e84ce932da7f1d1c5e719d207b0ad
Kyle Sykes <kyle.a.sykes@gmail.com>
2019-04-12 22:19:21 -0500

    Rearrange tests

--- awslambda/tests/test_connection.py
+++ awslambda/tests/test_connection.py
@@ -1,51 +1,7 @@
-import json
 import pytest
 
 from api.connection import connections
 
 
-@pytest.fixture()
-def sample_body():
-    return {"body": "test"}
-
-
-@pytest.fixture()
-def sample_connection_response():
-    return {
-        "statusCode": 200,
-        "body": json.dumps(
-            [
-                {
-                    "UID": 1,
-                    "Organization": 1,
-                    "Name": "SF",
-                    "CreatedDate": "2019-03-09 20:42:03",
-                    "CreatedBy": 1,
-                    "Type": "A",
-                    "ConnectionInfo": "{conn string}",
-                },
-                {
-                    "UID": 2,
-                    "Organization": 1,
-                    "Name": "CW",
-                    "CreatedDate": "2019-03-10 04:42:03",
-                    "CreatedBy": 1,
-                    "Type": "B",
-                    "ConnectionInfo": "{conn string}",
-                },
-                {
-                    "UID": 6,
-                    "Organization": 1,
-                    "Name": "Secure Download",
-                    "CreatedDate": "2019-03-23 20:42:03",
-                    "CreatedBy": 0,
-                    "Type": "F",
-                    "ConnectionInfo": 0,
-                },
-            ]
-        ),
-    }
-
-
-def test_connections_function(sample_body, sample_connection_response):
-    assert connections(sample_body, None) == sample_connection_response
+def test_connections_function(sample_event, sample_connection_response):
+    assert connections(sample_event, None) == sample_connection_response
