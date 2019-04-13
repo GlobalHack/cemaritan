4d5e76f1362843d268072be34bcd05e570085936
Matt Pitlyk <matt.pitlyk@gmail.com>
2019-04-12 22:18:30 -0500

    Moved models dirctory

--- models/__init__.py
+++
@@ -1,5 +0,0 @@
-from models.Connection import Connection
-from models.DataMapping import DataMapping
-from models.Organization import Organization
-from models.Transfer import Transfer
-from models.User import User
