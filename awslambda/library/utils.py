1f5bffd830c9719fd2fb649c0ec79f931dd4c06c
Matt Pitlyk <matt.pitlyk@gmail.com>
2019-04-22 16:02:48 -0500

    Fixed requriments.txt to install psycog2-binary

--- awslambda/library/utils.py
+++ awslambda/library/utils.py
@@ -3,10 +3,10 @@ import functools
 
 
 
-def awshandler(function):
-    """A decorator to wrap AWS Lambda function hanlder in a
-    try-catch that ensure returning a proper error.
-    """
-    @functools.wraps(function)
-    def wrapper(function):
+# def awshandler(function):
+#     """A decorator to wrap AWS Lambda function hanlder in a
+#     try-catch that ensure returning a proper error.
+#     """
+#     @functools.wraps(function)
+#     def wrapper(function):
         
