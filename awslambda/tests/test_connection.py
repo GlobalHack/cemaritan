c8280163690b0976d27ecd35129dd68cb8513015
Kyle Sykes <kyle.a.sykes@gmail.com>
2019-04-29 16:48:40 -0500

    Rename some functions, add test for single connection route, add rough symlinking

--- awslambda/tests/test_connection.py
+++ awslambda/tests/test_connection.py
@@ -1,7 +1,16 @@
 import pytest
 
-from api.connection import connections
+from api.connection import connections, get_connection
 
 
-def test_connections_function(connections_event, sample_connection_response):
-    assert connections(connections_event, None) == sample_connection_response
+def test_connections_function(connections_event, sample_connections_response):
+    assert connections(connections_event, None) == sample_connections_response
+
+
+def test_connection_single_function(
+    connection_single_event, sample_connection_single_response
+):
+    assert (
+        get_connection(connection_single_event, None)
+        == sample_connection_single_response
+    )
