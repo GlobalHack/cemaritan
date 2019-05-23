25cc43f31f7e380ea126693f7d2be7908bf26616
Kyle Sykes <kyle.a.sykes@gmail.com>
2019-05-23 15:40:14 -0500

    Adding in stuff to create history from transfer

--- awslambda/api/transfer.py
+++ awslambda/api/transfer.py
@@ -5,9 +5,10 @@ import library.db_queries as db_queries
 
 from library.exceptions import DatabaseReturnedNone
 from library.db_connections import Postgres
-from library.utils import awshandler, aws_get_path_parameter
+from library.utils import awshandler, aws_get_path_parameter, aws_get_json_body
 
 conn = Postgres()
+
 
 @awshandler
 def transfers(event, context):
@@ -18,6 +19,40 @@ def transfers(event, context):
 
 @awshandler
 def create_transfer(event, context):
+    # parse transfer content
+    body = aws_get_json_body(event)
+    user_id = body["user_id"]
+    name = body["name"]
+    created_date = body["created_date"]
+    created_by = body["created_by"]
+    organization_id = body["organization_id"]
+    source = body["source"]
+    source_mapping = body["source_mapping"]
+    destination = body["destination"]
+    destination_mapping = body["destination_mapping"]
+    start_date_time = body["start_date_time"]
+    frequency = body["frequency"]
+    record_filter = body["record_filter"]
+    active = body["active"]
+    # create transfer object
+    db_queries.create_transfer(
+        connection=conn,
+        user_id=user_id,
+        name=name,
+        created_date=created_date,
+        created_by=created_by,
+        organization_id=organization_id,
+        source=source,
+        source_mapping=source_mapping,
+        destination=destination,
+        destination_mapping=destination_mapping,
+        start_date_time=start_date_time,
+        frequency=frequency,
+        record_filter=record_filter,
+        active=active,
+    )
+    # create corresponding history object
+    db_queries.create_history()
     return "Data not saved yet."
 
 
@@ -25,7 +60,9 @@ def create_transfer(event, context):
 def get_transfer(event, context):
     organization_id = aws_get_path_parameter(event, "organization_id")
     transfer_id = aws_get_path_parameter(event, "transfer_id")
-    transfer = db_queries.get_transfer(connection=conn, organization_id=organization_id, transfer_id=transfer_id)
+    transfer = db_queries.get_transfer(
+        connection=conn, organization_id=organization_id, transfer_id=transfer_id
+    )
     if transfer is None:
         raise DatabaseReturnedNone(f"Check object id: {transfer_id}")
     return transfer.to_dict()
--- awslambda/library/utils.py
+++ awslambda/library/utils.py
@@ -1,36 +1,55 @@
-
 import functools
 import json
 
 from library.exceptions import DatabaseReturnedNone
 
+
 def awshandler(function):
     """A decorator to wrap AWS Lambda function hanlder in a
     try-catch that ensure returning a proper error.
     """
+
     @functools.wraps(function)
     def wrapper(*args, **kwargs):
         try:
             payload = function(*args, **kwargs)
         except DatabaseReturnedNone as e:
-            return {"statusCode": 400, "headers": {"Access-Control-Allow-Origin": "*"},  "body": json.dumps({'message': str(e)})}
+            return {
+                "statusCode": 400,
+                "headers": {"Access-Control-Allow-Origin": "*"},
+                "body": json.dumps({"message": str(e)}),
+            }
         except Exception as e:
             # TODO: Rethink what to return...dumping exceptions is scary for data leakage
-            return {"statusCode": 400, "headers": {"Access-Control-Allow-Origin": "*"},  "body": "400 Bad Request\n\n" + json.dumps(str(e))}
-        return {"statusCode": 200, "headers": {"Access-Control-Allow-Origin": "*"},  "body": json.dumps(payload)}
+            return {
+                "statusCode": 400,
+                "headers": {"Access-Control-Allow-Origin": "*"},
+                "body": "400 Bad Request\n\n" + json.dumps(str(e)),
+            }
+        return {
+            "statusCode": 200,
+            "headers": {"Access-Control-Allow-Origin": "*"},
+            "body": json.dumps(payload),
+        }
+
     return wrapper
-        
+
 
 def aws_get_http_method(event):
     """Return the http method from the AWS Lambda event object."""
-    return event['requestContext']['httpMethod']
+    return event["requestContext"]["httpMethod"]
 
 
 def aws_get_path_parameters(event):
     """Extract path parameters from AWS Lambda event object."""
-    return event['pathParameters']
+    return event["pathParameters"]
 
 
 def aws_get_path_parameter(event, parameter):
     """Extract specific path parameter from AWS Lambda event object."""
     return aws_get_path_parameters(event)[parameter]
+
+
+def aws_get_json_body(event):
+    """Returns the body from a POST/PUT AWS Lambda event object"""
+    return json.loads(event["body"])
