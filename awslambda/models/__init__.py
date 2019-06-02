6c5f549a077fef1a6237cc767c1f506c4705c8da
Matt Pitlyk <matt.pitlyk@gmail.com>
2019-06-01 23:22:21 -0500

    Added new Models to __init__.py export.

--- awslambda/models/__init__.py
+++ awslambda/models/__init__.py
@@ -1,7 +1,9 @@
-from models.Connection import Connection
-from models.Mapping import Mapping
-from models.Organization import Organization
-from models.Transfer import Transfer
-from models.User import User
-from models.History import History
-from models.Download import Download
+# from models.Connection import Connection
+# from models.Mapping import Mapping
+# from models.Organization import Organization
+# from models.Transfer import Transfer
+# from models.User import User
+# from models.History import History
+# from models.Download import Download
+
+from models.Models import *
