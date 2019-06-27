03f0340bb0957ff5c7fce943febfb8f6c6213b48
Matt Pitlyk <matt.pitlyk@gmail.com>
2019-06-27 16:30:57 -0500

    Built out connection classes.

--- awslambda/dataexchange/__init__.py
+++ awslambda/dataexchange/__init__.py
@@ -20,14 +20,67 @@ def get_transfers_for_org(event, context):
     for t in transfers:
         msg = {"transfer": t.to_dict()}
         lambda_client.invoke(FunctionName='aws-cemaritan-dataexchange-dev-do_transfer',
-                             InvocationType='Event',
-                             Payload=json.dumps(msg))
+                                InvocationType='Event',
+                                Payload=json.dumps(msg))
     return {"Number of transfers": len(transfers)}
-
-
 
 
 
 def do_transfer(event, context):
     print(event)
+
+
+### Connections ###
+
+class Connection:
+
+    def __init__(self, mapping):
+        self.mapping = mapping
+
+    def get_data(self):
+        raise NotImplementedError()
+
+    def convert(self):
+        raise NotImplementedError()
+
+    def send_data(self):
+        raise NotImplementedError()
     
+
+class Upload(Connection):
+    """Can be a source, but not a destination.
+    """
+    def __init__(self, mapping, location: str):
+        super().__init__(self, mapping=mapping)
+        self.location = location
+    
+    def get_data(self):
+        s3_client = boto3.client('s3')
+
+
+class Download(Connection):
+    """Can be a destination, but not a source.
+    """
+    def __init__(self, mapping, location: str):
+        super().__init__(self, mapping=mapping)
+        self.location = location
+
+    def get_data(self):
+        pass
+
+    def convert(self):
+        if mapping is not None:
+            # Convert
+            pass
+
+    def put_data(self):
+        s3_client = boto3.client('s3')
+        s3.Object('cemaritan-dev-test', f'test_{rand_int}.txt').put(Body='test body')
+
+
+class Salesforce(Connection):
+    """Can be both a source and a destination.
+    """
+    def __init__(self, mapping=mapping):
+        super().__init__(self, mapping=mapping)
+        
